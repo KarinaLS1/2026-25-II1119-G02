28c56ec7e7bf3d4e42b8e3eb52c3bc121a5e8e82
KarinaLS1 <KARINA.LOPEZSALAZAR@ucr.ac.cr>
2026-09-06 11:47:55 -0600

    Add logic to evaluate piece approval based on tests

--- ejercicio1.py
+++ ejercicio1.py
@@ -3,3 +3,11 @@ prueba_dimensión:boolean
 prueba_dureza:boolean 
 pieza_metálica:boolean  
 #Entradas
+prueba_dimensión = true 
+prueba_dureza = true 
+pieza_metálica = prueba_dimensión + prueba_dureza
+#Salidas
+if prueba_dimensión AND prueba_dureza
+print("pieza aprobada") 
+else 
+print("pieza rechazada")
